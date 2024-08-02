ba697fcde6ca81936c099be6a3dd72b61f0e335c
chathamabate <chathamabate@gmail.com>
2024-08-01 22:07:06 -0600

    Linked List implementation

--- include/chutil/list.h
+++ include/chutil/list.h
@@ -41,7 +41,7 @@ static inline const void *al_get(array_list_t *al, size_t i) {
 }
 
 static inline void al_get_copy(array_list_t *al, size_t i, void *dest) {
-    memcpy(dest, al_get_mut(al, i), al->cell_size);
+    memcpy(dest, al_get(al, i), al->cell_size);
 }
 
 static inline void al_set(array_list_t *al, size_t i, const void *src) {
@@ -50,5 +50,56 @@ static inline void al_set(array_list_t *al, size_t i, const void *src) {
 
 void al_push(array_list_t *al, const void *src);
 void al_pop(array_list_t *al, void *dest);
+void al_poll(array_list_t *al, void *dest);
+
+typedef struct _linked_list_node_hdr_t {
+    struct _linked_list_node_hdr_t *prev;
+    struct _linked_list_node_hdr_t *next;
+} linked_list_node_hdr_t;
+
+static inline void *llnh_get_cell(linked_list_node_hdr_t *llnh) {
+    return llnh + 1;
+}
+
+typedef struct _linked_list_t {
+    size_t cell_size;
+    size_t len;
+
+    linked_list_node_hdr_t *first;
+    linked_list_node_hdr_t *last;
+} linked_list_t;
+
+linked_list_t *new_linked_list(size_t cs);
+void delete_linked_list(linked_list_t *ll);
+
+static inline size_t ll_len(linked_list_t *ll) {
+    return ll->len;
+}
+
+static inline size_t ll_cell_size(linked_list_t *ll) {
+    return ll->cell_size;
+}
+
+void *ll_get_mut(linked_list_t *ll, size_t i);
+
+static inline const void *ll_get(linked_list_t *ll, size_t i) {
+    return ll_get_mut(ll, i);
+}
+
+static inline void ll_get_copy(linked_list_t *ll, size_t i, void *dest) {
+    memcpy(dest, ll_get(ll, i), ll->cell_size);
+}
+
+static inline void ll_set(linked_list_t *ll, size_t i, const void *src) {
+    memcpy(ll_get_mut(ll, i), src, ll->cell_size);
+}
+
+void ll_push(linked_list_t *ll, const void *src);
+void ll_pop(linked_list_t *ll, void *dest);
+void ll_poll(linked_list_t *ll, void *dest);
+
+
+
+
 
 #endif
--- src/list.c
+++ src/list.c
@@ -1,5 +1,6 @@
 #include "chutil/list.h"
 #include "chutil/debug.h"
+#include <string.h>
 
 array_list_t *new_array_list_w_cap(size_t cs, size_t cp) {
     if (cs == 0) {
@@ -45,4 +46,129 @@ void al_pop(array_list_t *al, void *dest) {
 
     al->len--;
 }
+
+void al_poll(array_list_t *al, void *dest) {
+    if (al->len == 0) {
+        return;
+    }
+
+    if (dest) {
+        memcpy(dest, al->arr, al->cell_size);
+    }
+
+    uint8_t *curr = al_get_mut(al, 0);
+    uint8_t *next = al_get_mut(al, 1);
+    const uint8_t *end = al_get_mut(al, al->len - 1);
+
+    while (next <= end) {
+        memcpy(curr, next, al->cell_size);
+        curr = next;
+        next += al->cell_size;
+    }
+
+    al->len--;
+}
+
+linked_list_t *new_linked_list(size_t cs) {
+    if (cs == 0) {
+        return NULL;
+    }
+
+    linked_list_t *ll = safe_malloc(sizeof(linked_list_t));
+    ll->cell_size = cs;
+    ll->len = 0;
+    ll->first = NULL;
+    ll->last = NULL;
+
+    return ll;
+}
+
+void delete_linked_list(linked_list_t *ll) {
+    linked_list_node_hdr_t *curr = ll->first;
+    linked_list_node_hdr_t *next;
+
+    while (curr) {
+        next = curr->next;
+        safe_free(curr);
+
+        curr = next;
+    }
+
+    safe_free(ll);
+}
+
+void *ll_get_mut(linked_list_t *ll, size_t i) {
+    linked_list_node_hdr_t *node = ll->first;
+    size_t cnt = 0;
+
+    while (node && i < cnt) {
+        node = node->next;
+        cnt++;
+    }
+
+    if (node) {
+        return llnh_get_cell(node);
+    }
+
+    return NULL;
+}
+
+void ll_push(linked_list_t *ll, const void *src) {
+    linked_list_node_hdr_t *node = 
+        safe_malloc(sizeof(linked_list_node_hdr_t) + ll->cell_size);
+
+    node->next = NULL;
+    node->prev = ll->last;
+
+    if (ll->last) {
+        ll->last->next = node;
+    } else {
+        ll->first = node;
+    }
+
+    ll->last = node;
+    ll->len++;
+
+    void *cell = llnh_get_cell(node);
+    memcpy(cell, src, ll->cell_size);
+}
+
+void ll_pop(linked_list_t *ll, void *dest) {
+    if (ll->len == 0) {
+        return;
+    }
+
+    if (dest) {
+        // Last cannot be NULL here.
+        void *cell = llnh_get_cell(ll->last);
+        memcpy(dest, cell, ll->cell_size);
+    }
+
+    ll->last = ll->last->prev;
+    if (ll->last) {
+        ll->last->next = NULL;
+    }
+
+    ll->len--;
+}
+
+void ll_poll(linked_list_t *ll, void *dest) {
+    if (ll->len == 0) {
+        return;
+    }
+
+    if (dest) {
+        void *cell = llnh_get_cell(ll->first);
+        memcpy(dest, cell, ll->cell_size);
+    }
+
+    ll->first = ll->first->next;
+    if (ll->first) {
+        ll->first->prev = NULL;
+    }
+
+    ll->len--;
+}
+
+
 
