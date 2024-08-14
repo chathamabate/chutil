b0115d0989bc9aea2bb8c529a317d313e312fb25
chathamabate <chathamabate@gmail.com>
2024-08-13 18:05:37 -0600

    Progress on new make format

--- Makefile
+++ Makefile
@@ -1,63 +1,7 @@
-CC:=gcc
-FLAGS:=-Wall -Wextra -Wpedantic -std=c11
 
-CUR_DIR:=$(shell pwd)
-ABS_BUILD_PATH:=$(CUR_DIR)/build
+LIB_NAME	:= chutil
+DEPS		:=
+SRCS		:= debug.c list.c map.c
+TEST_SRCS   := list.c map.c main.c
 
-LIB_NAME:=chutil
-LIB_FILE:=build/lib$(LIB_NAME).a
-
-# Dependencies found in clib (my local store of C dependencies)
-DEPS:=unity
-DEPS_PATHS:=$(addprefix /Users/chathamabate/clib/,$(DEPS))
-DEPS_FLAGS:=$(foreach dp,$(DEPS_PATHS),-L$(dp)) $(foreach dep,$(DEPS),-l$(dep))
-
-REL_INC_PATHS:=include
-DEPS_INC_PATHS:=$(addsuffix /include,$(DEPS_PATHS))
-ABS_INC_PATHS:=$(DEPS_INC_PATHS) $(addprefix $(CUR_DIR)/,$(REL_INC_PATHS))
-
-INC_FLAGS:=$(addprefix -I,$(ABS_INC_PATHS))
-
-HEADERS:=$(wildcard include/chutil/*.h)
-
-SRCS:=debug.c list.c map.c
-
-OBJS:=$(patsubst %.c,%.o,$(SRCS))
-FULL_OBJS:=$(addprefix build/,$(OBJS))
-
-TEST_SRCS:=main.c list.c map.c
-
-TEST_HEADER:=$(wildcard test/*.h)
-TEST_OBJS:=$(patsubst %.c,%.o,$(TEST_SRCS))
-FULL_TEST_OBJS:=$(addprefix build/test/,$(TEST_OBJS))
-
-.PHONY: all clean clangd
-
-all: $(LIB_FILE) build/test/test
-
-build:
-	mkdir build
-
-build/%.o: src/%.c $(HEADERS) | build
-	$(CC) -c $(FLAGS) $(INC_FLAGS) $< -o $@
-
-$(LIB_FILE): $(FULL_OBJS)
-	ar rcs $@ $^ 
-
-build/test:
-	mkdir build/test
-
-build/test/%.o: test/%.c $(HEADERS) $(TEST_HEADERS) | build/test
-	$(CC) -c $(FLAGS) $(INC_FLAGS) $< -o $@
-
-build/test/test: $(FULL_TEST_OBJS) $(LIB_FILE)
-	$(CC) $^ $(DEPS_FLAGS) -L$(ABS_BUILD_PATH) -lchutil -o $@
-
-clangd:
-	echo "CompileFlags:" > .clangd
-	echo "  Add:" >> .clangd
-	$(foreach flag,$(INC_FLAGS),echo "    - $(flag)" >> .clangd;) 
-
-clean:
-	rm -rf build
-
+include ../stub.mk
--- stub.mk
+++ stub.mk
@@ -1,5 +1,4 @@
 
-
 # This stub file is meant to be included
 # at the bottom of every library's make file.
 # 
@@ -21,31 +20,84 @@
 # Use the install target to copy build library and headers
 
 PROJ_DIR:=$(shell git rev-parse --show-toplevel)
-LIB_DIR:=$(PROJ_DIR)/$(LIB_NAME)
+
+# I like absolute paths tbh.
+LIB_DIR		:=$(PROJ_DIR)/$(LIB_NAME)
+
+INCLUDE_DIR	:=$(LIB_DIR)/include
+SRC_DIR		:=$(LIB_DIR)/src
+TEST_DIR	:=$(LIB_DIR)/test
+
+BUILD_DIR		:=$(LIB_DIR)/build
+BUILD_TEST_DIR	:=$(BUILD_DIR)/test
+
+LIB_FILE_NAME	:=lib$(LIB_NAME).a
+LIB_FILE		:=$(BUILD_DIR)/$(LIB_FILE_NAME)
 
 include $(PROJ_DIR)/vars.mk
 
 # Each library will be built entirely independently to its
 # dependencies. It will search for libraries in the install path.
 
-HEADERS		:=$(wildcard include/*.h)
+HEADERS		:=$(wildcard $(INCLUDE_DIR)/*.h)
 OBJS		:=$(patsubst %.c,%.o,$(SRCS))
-FULL_OBJS	:=$(addprefix build/,$(OBJS))
+FULL_OBJS	:=$(addprefix $(BUILD_DIR)/,$(OBJS))
 
-TEST_HEADERS	:=$(wildcard test/*.h)
+TEST_HEADERS	:=$(wildcard $(TEST_DIR)/*.h)
 TEST_OBJS		:=$(patsubst %.c,%.o,$(TEST_SRCS))
-FULL_TEST_OBJS	:=$(addprefix build/test/,$(TEST_OBJS))
+FULL_TEST_OBJS	:=$(addprefix $(BUILD_TEST_DIR)/test,$(TEST_OBJS))
 
-INCLUDE_PATHS	:=$(LIB_DIR)/include
-INCLUDE_PATHS	+=$(INSTALL_DIR)/include
+# It is important our local include is first.
+# This should be searched first.
+INCLUDE_PATHS	:=$(INCLUDE_DIR) $(INSTALL_DIR)/include
 INCLUDE_FLAGS	:=$(addprefix -I,$(INCLUDE_PATHS))
 
 # Where to look for static library dependencies.
-DEPS_PATHS	:=$(INSTALL_DIR)
-DEPS_FLAGS	:=$(addprefix -L,$(DEPS_PATHS))
-DEPS_FLAGS	+=$(foreach dep,$(DEPS),-l$(dep))
+# Again, it is important our local build is first.
+DEPS_PATHS 	:=$(BUILD_DIR) $(INSTALL_DIR)
+DEPS_FLAGS	:=$(addprefix -L,$(DEPS_PATHS)) $(foreach dep,$(DEPS),-l$(dep))
 
-build:
-	mkdir build
+.PHONY: all lib test uninstall install clean clangd
 
-build/%.o:
+all: $(LIB_FILLE)
+
+lib: $(LIB_FILE)
+
+test: $(BUILD_TEST_DIR)/test
+
+uninstall:
+	rm -rf $(INSTALL_DIR)/include/$(LIB_NAME)
+	rm -f $(INSTALL_DIR)/$(LIB_FILE_NAME)
+
+install: uninstall $(LIB_FILE)
+	cp -r $(INCLUDE_DIR) $(INSTALL_DIR)/include
+	cp $(LIB_FILE) $(INSTALL_DIR)
+
+clean:
+	rm -rf $(BUILD_DIR)
+
+clangd:
+	echo "CompileFlags:" > .clangd
+	echo "  Add:" >> .clangd
+	$(foreach flag,$(INCLUDE_FLAGS),echo "    - $(flag)" >> .clangd;) 
+
+$(BUILD_DIR):
+	mkdir $@
+
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.c $(HEADERS) | $(BUILD_DIR)
+	$(CC) -c $(FLAGS) $(INCLUDE_FLAGS) $< -o $@
+
+# Remember, the lib file doesn't actually care about dependencies.
+$(LIB_FILE): $(FULL_OBJS)
+	ar rcs $@ $^
+
+$(BUILD_TEST_DIR):
+	mkdir $@
+
+$(BUILD_TEST_DIR)/%.o: $(TEST_DIR)/%.c $(HEADERS) $(TEST_HEADERS) | $(BUILD_TEST_DIR)
+	$(CC) -c $(FLAGS) $(INCLUDE_FLAGS) $< -o $@
+
+# Always include unity!
+$(BUILD_TEST_DIR)/test: $(FULL_TEST_OBJS) $(LIB_FILE)
+	$(CC) $(DEPS_FLAGS) -lunity $^ -o $@
+
