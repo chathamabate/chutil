f6ceaaf59c55c6b90690ef29cf7f95981bc99a80
chathamabate <chathamabate@gmail.com>
2024-08-17 12:53:00 -0600

    Heap pop draft

--- chutil/src/heap.c
+++ chutil/src/heap.c
@@ -40,6 +40,10 @@ void delete_heap(heap_t *hp) {
 }
 
 static inline heap_val_header_t *hp_get_header(heap_t *hp, size_t i) {
+    if (i >= hp->len) {
+        return NULL;
+    }
+
     return (heap_val_header_t *)((uint8_t *)(hp->table) + (hp->cell_size * i));
 }
 
@@ -48,7 +52,63 @@ void *hp_peek(heap_t *hp) {
 }
 
 bool hp_pop(heap_t *hp, void *dest) {
-    return false;
+    if (hp->len == 0) {
+        return false;
+    }
+
+    // If we have elements, the root will always be what's copied.
+    heap_val_header_t *root_hdr = hp_get_header(hp, 0);
+    memcpy(dest, hvh_to_hv(root_hdr), hp->val_size);
+
+    if (hp->len == 1) {
+        hp->len--;
+        return true;
+    }
+
+    // This is conceptually swapped into the root position.
+    // After doing bubbling down, the correct position for this to be
+    // copied into will be found.
+    //
+    // We won't do the copy until the very end.
+    heap_val_header_t *last_hdr = hp_get_header(hp, hp->len - 1);
+    size_t p = last_hdr->priority;
+
+    // Shrink the valid area of our heap.
+    hp->len--;
+
+    size_t i = 0;
+
+    while (true) {
+        heap_val_header_t *curr_hdr = hp_get_header(hp, i);
+
+        size_t left = (i * 2) + 1;
+        heap_val_header_t *left_hdr = hp_get_header(hp, left);
+
+        size_t right = (i * 2) + 2;
+        heap_val_header_t *right_hdr = hp_get_header(hp, right);
+
+        if (left_hdr && left_hdr->priority < p) {
+            memcpy(curr_hdr, left_hdr, hp->cell_size); 
+            i = left;
+            continue;
+        }
+
+        if (right_hdr && right_hdr->priority < p) {
+            memcpy(curr_hdr, right_hdr, hp->cell_size); 
+            i = right;
+            continue;
+        }
+        
+        // If we make it here, there is either no left/right children.
+        // Or our priority higher than our children.
+        break;
+    }
+
+    // Now, we copy our "last" cell into i.
+    heap_val_header_t *spot_hdr = hp_get_header(hp, i);
+    memcpy(spot_hdr, last_hdr, hp->cell_size);
+
+    return true;
 }
 
 void hp_push(heap_t *hp, const void *src) {
@@ -86,5 +146,7 @@ void hp_push(heap_t *hp, const void *src) {
     heap_val_header_t *spot_hdr = hp_get_header(hp, i);
     spot_hdr->priority = p;
     memcpy(hvh_to_hv(spot_hdr), src, hp->val_size);
+
+    hp->len++;
 }
 
