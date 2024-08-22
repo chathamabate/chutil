5a86fb92e4fd90ca1d833c1e6aa9cbcd89d51a1d
chathamabate <chathamabate@gmail.com>
2024-08-21 19:52:31 -0600

    List equals progress

---
+++ chutil/src/list_helpers.c
@@ -0,0 +1,12 @@
+
+#include "chutil/list_helpers.h"
+#include "chutil/list.h"
+
+bool l_equals(list_t *l1, list_t *l2, list_cell_equals_ft eq) {
+    if (l_len(l1) != l_len(l2)) {
+        return false;
+    }
+
+    
+ 
+}
