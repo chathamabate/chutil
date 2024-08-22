74b7ba210485ba0d4a0fcb3a6337c68638f1acc9
chathamabate <chathamabate@gmail.com>
2024-08-21 19:54:52 -0600

    List EQs impl done

--- chutil/src/list_helpers.c
+++ chutil/src/list_helpers.c
@@ -7,6 +7,19 @@ bool l_equals(list_t *l1, list_t *l2, list_cell_equals_ft eq) {
         return false;
     }
 
-    
- 
+    const void *cell1;
+    const void *cell2;
+
+    l_reset_iterator(l1);
+    l_reset_iterator(l2);
+
+    // NOTE: we really only need to check one of these not being NULL
+    // because we know the lengths are equal, but whatevs.
+    while ((cell1 = l_next(l1)) && (cell2 = l_next(l2))) {
+        if (!eq(cell1, cell2)) {
+            return false;
+        }
+    }
+
+    return true;
 }
