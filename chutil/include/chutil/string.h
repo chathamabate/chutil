0d8e8708a761ea3fe95d22d86451b48c257ce25b
chathamabate <chathamabate@gmail.com>
2024-08-22 22:19:24 -0600

    Smart String First Draft Implementation

--- chutil/include/chutil/string.h
+++ chutil/include/chutil/string.h
@@ -5,15 +5,33 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct _shared_string_t {
+    // The number of people using this string at the moment.
+    size_t ref_count;
+
+    size_t cap;
+    size_t len;
+    char *buf;
+} shared_string_t;
+
+// This makes sure the length is only calcuated once of a literal.
+typedef struct _shared_literal_t {
+    size_t ref_count;
+    size_t len;
+    const char *literal;
+} shared_literal_t;
+
 typedef struct _string_t {
-    size_t cap;  // how large data is.
-    size_t len;  // number of non-null bytes in our string.
-                 // data[len] should always point to '\0'.
-    char *data;
+    bool from_literal; // Which ever 
+    union {
+        shared_literal_t *sl;
+        shared_string_t *ss; 
+    };
 } string_t;
 
 string_t *new_string(void);
 string_t *new_string_from_cstr(const char *cstr);
+string_t *new_string_from_literal(const char *literal);
 void delete_string(string_t *s);
 
 bool s_equals(const string_t *s1, const string_t *s2);
@@ -31,35 +49,34 @@ static inline uint32_t s_indirect_hash(const string_t * const *s) {
 }
 
 static inline size_t s_len(const string_t *s) {
-    return s->len;
+    return s->from_literal ? s->sl->len : s->ss->len;
 }
 
 // These both return entirely new strings!
 // end is exclusive.
 string_t *s_substring(const string_t *s, size_t start, size_t end);
-
-static inline string_t *s_copy(const string_t *s) {
-    return new_string_from_cstr(s->data);
-}
+string_t *s_copy(const string_t *s);
 
 void s_append_char(string_t *s, char c);
 void s_append_cstr(string_t *s, const char *addition);
 
+// make sure you don't use the returned cstr for longer than s's lifetime!
+// Also, do not modify the returned cstring! This function is meant for
+// conveint reading only!
+static inline const char *s_get_cstr(const string_t *s) {
+    return s->from_literal ? s->sl->literal : s->ss->buf;
+}
+
 static inline void s_append_string(string_t *s, const string_t *addition) {
-    s_append_cstr(s, addition->data);
+    s_append_cstr(s, s_get_cstr(addition));
 }
 
-// As you can see...these don't do safety checks. Be careful!
-static inline const char *s_get_cstr(string_t *s) {
-    return s->data;
-}
 
-static inline void s_set_char(string_t *s, size_t i, char c) {
-    s->data[i] = c;
-}
+// Neither of these check the index, so be careful!
+void s_set_char(string_t *s, size_t i, char c);
 
 static inline char s_get_char(const string_t *s, size_t i) {
-    return s->data[i];
+    return s_get_cstr(s)[i];
 }
 
 #endif
--- chutil/src/string.c
+++ chutil/src/string.c
@@ -4,13 +4,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Versatile constructor for shared_string.
+// If cstr = NULL, len will be set to 0, and cap to 1.
+// Otherwise, if cap < len + 1, cap will be set to len + 1.
+static shared_string_t *new_shared_string(const char *cstr, size_t len, size_t cap) {
+    if (!cstr) {
+        cap = 1;
+        len = 0;
+    } else if (cap < len + 1) {
+        cap = len + 1;
+    }
+
+    shared_string_t *ss = (shared_string_t *)safe_malloc(sizeof(shared_string_t));
+    ss->ref_count = 1;
+    ss->cap = cap;
+    ss->len = len;
+    ss->buf = (char *)safe_malloc(sizeof(char) * ss->cap);
+
+    if (len > 0) {
+        memcpy(ss->buf, cstr, len);
+    }
+    ss->buf[ss->len] = '\0';
+
+    return ss;
+}
+
 string_t *new_string(void) {
     string_t *s = (string_t *)safe_malloc(sizeof(string_t));     
-
-    s->cap = 1;
-    s->len = 0;
-    s->data = (char *)safe_malloc(sizeof(char) * s->cap);
-    s->data[0] = '\0';
+    s->from_literal = false;
+    s->ss = new_shared_string(NULL, 0, 1);
 
     return s;
 }
@@ -21,25 +43,54 @@ string_t *new_string_from_cstr(const char *cstr) {
     }
 
     string_t *s = (string_t *)safe_malloc(sizeof(string_t));     
+    s->from_literal = false;
 
-    size_t len = strlen(cstr);
-    s->cap = len + 1;
-    s->len = len;
-    s->data = (char *)safe_malloc(sizeof(char) * s->cap);
-
-    strcpy(s->data, cstr);
+    size_t cstr_len = strlen(cstr);
+    s->ss = new_shared_string(cstr, cstr_len, cstr_len + 1);
 
     return s;
-    
+}
+
+string_t *new_string_from_literal(const char *literal) {
+    if (!literal) {
+        return new_string();
+    }
+
+    // Should be the only place we construct a shared_literal.
+    shared_literal_t *sl = (shared_literal_t *)safe_malloc(sizeof(shared_literal_t));
+    sl->ref_count = 1;
+    sl->len = strlen(literal);
+    sl->literal = literal;
+
+    string_t *s = (string_t *)safe_malloc(sizeof(string_t));
+    s->from_literal = true;
+    s->sl = sl;
+
+    return s;
+}
+
+static void s_release_shared(string_t *s) {
+    if (s->from_literal) {
+        s->sl->ref_count--;
+        if (s->sl->ref_count == 0) {
+            safe_free(s->sl);
+        }
+    } else {
+        s->ss->ref_count--;
+        if (s->ss->ref_count == 0) {
+            safe_free(s->ss->buf);
+            safe_free(s->ss);
+        }
+    }
 }
 
 void delete_string(string_t *s) {
-    safe_free(s->data);
+    s_release_shared(s);
     safe_free(s);
 }
 
 bool s_equals(const string_t *s1, const string_t *s2) {
-    return strcmp(s1->data, s2->data) == 0;
+    return strcmp(s_get_cstr(s1), s_get_cstr(s2)) == 0;
 }
 
 uint32_t s_hash(const string_t *s) {
@@ -52,54 +103,127 @@ uint32_t s_hash(const string_t *s) {
 }
 
 string_t *s_substring(const string_t *s, size_t start, size_t end) {
-    if (end > s->len) {
-        end = s->len;
+    size_t cstr_len = s_len(s);
+    const char *cstr = s_get_cstr(s);
+
+    if (end > cstr_len) {
+        end = cstr_len;
     }
 
     if (end <= start) {
         return new_string();
     }
 
+    if (start == 0 && end == cstr_len) {
+        return s_copy(s);
+    }
+
     size_t len = end - start;
+    shared_string_t *ss = new_shared_string(cstr, len, len + 1);
 
     string_t *sub_s = (string_t *)safe_malloc(sizeof(string_t));
-    sub_s->cap = len + 1;
-    sub_s->len = len;
-    sub_s->data = (char *)safe_malloc(sizeof(char) * sub_s->cap);
-
-    memcpy(sub_s->data, &(s->data[start]), len);
-    sub_s->data[sub_s->len] = '\0';
+    sub_s->from_literal = false;
+    sub_s->ss = ss;
     
     return sub_s;
 }
 
-// Take s, and expand it to have a capacity of at least min_cap.
-static void s_expand(string_t *s, size_t min_cap) {
-    if (s->cap >= min_cap) {
+string_t *s_copy(const string_t *s) {
+    string_t *copy = (string_t *)safe_malloc(sizeof(string_t));
+    copy->from_literal = s->from_literal;
+
+    if (s->from_literal) {
+        copy->sl = s->sl;
+        s->sl->ref_count++;
+    } else {
+        copy->ss = s->ss;
+        s->ss->ref_count++;
+    }
+
+    return copy;
+}
+
+// Basically, we are going to modify the characters pointed to by s.
+// After this call, we need to make sure s points to a buffer which is
+// not pointed to by other strings, the capacity of said buffer is at least
+// min_cap, AND s does NOT point to a string literal.
+static void s_prepare_modify(string_t *s, size_t min_cap) {
+    size_t len;
+    shared_string_t *ss;
+
+    // Length of underyling. 
+    len = s_len(s);
+
+    // Our minimum capacity will always be rounded up to
+    // the length of the underlying string + 1.
+    if (min_cap < len + 1) {
+        min_cap = len + 1;
+    }
+
+    if (s->from_literal) {
+        // We are preparing to our first modification off of a 
+        // string literal, usage of the min_cap as is will be fine.
+        ss = new_shared_string(s->sl->literal, len, min_cap);
+        s_release_shared(s);
+        s->ss = ss;
+
+        return;
+    } 
+
+    if (s->ss->ref_count > 1) {
+        // Similar to string literal, if we must create a new
+        // shared string, just use min_cap as is.
+        ss = new_shared_string(s->ss->buf, len, min_cap);
+        s_release_shared(s);
+        s->ss = ss;
+
         return;
     }
 
-    // for now, we'll just do a simple double.
-    s->cap = min_cap * 2;
-    s->data = safe_realloc(s->data, sizeof(char) * s->cap);
+    // If we make it here, we have ownership of an underlying buffer!
+    // check if a resize is needed!
+
+    if (s->ss->cap >= min_cap) {
+        return;
+    }
+
+    // RESIZE!!
+
+    // Try a simple double, otherwise just use the given min_cap.
+    size_t new_cap = s->ss->cap * 2;
+    if (min_cap > new_cap) {
+        new_cap = min_cap;
+    }
+
+    s->ss->buf = (char *)safe_realloc(s->ss->buf, new_cap);
+    s->ss->cap = new_cap;
 }
 
 void s_append_char(string_t *s, char c) {
-    size_t min_cap = s->len + 2; // c + \0.
-    s_expand(s, min_cap);
+    size_t min_cap = s_len(s) + 2;
+    s_prepare_modify(s, min_cap);
 
-    s->len++;
-    
-    s->data[s->len - 1] = c;
-    s->data[s->len] = '\0';
+    // After a prepare modify, we will never be pointing to 
+    // a literal.
+
+    s->ss->buf[s->ss->len] = c;
+    s->ss->buf[s->ss->len + 1] = '\0';
+
+    s->ss->len++;
 }
 
 void s_append_cstr(string_t *s, const char *addition) {
+    size_t len = s_len(s);
     size_t add_len = strlen(addition);
-    size_t min_cap = s->len + add_len + 1;
-    s_expand(s, min_cap);
+    size_t min_cap = len + add_len + 1;
+    s_prepare_modify(s, min_cap);
 
-    strcpy(&(s->data[s->len]), addition);
-    s->len = s->len + add_len;
+    strcpy(&(s->ss->buf[len]), addition);
+    s->ss->len = len + add_len;
 }
 
+void s_set_char(string_t *s, size_t i, char c) {
+    size_t len = s_len(s);   
+    s_prepare_modify(s, len + 1);
+    s->ss->buf[i] = c;
+}
