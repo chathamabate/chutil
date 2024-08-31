bb0874c1afdd89f1c140da83cbb31e0f2ee46eae
chathamabate <chathamabate@gmail.com>
2024-08-31 13:43:30 -0600

    Lookup Helpers

--- chjson/include/chjson/json_helpers.h
+++ chjson/include/chjson/json_helpers.h
@@ -15,4 +15,16 @@ string_t *json_to_string(json_t *json, bool spaced);
 
 bool json_equals(json_t *json1, json_t *json2);
 
+// The below three calls can accept NULL for the json element,
+// in this case NULL is returned.
+// This is to allow for chaining.
+
+// NOTE: there is no key_literal here... the key cstr given will
+// not be modified in anyway or stored anywhere. 
+//
+// It will temporarily be treated as a literal for efficiency.
+json_t *json_lookup_key_cstr(json_t *json, const char *key_cstr);
+json_t *json_lookup_key(json_t *json, string_t *key);
+json_t *json_lookup_index(json_t *json, size_t ind);
+
 #endif
